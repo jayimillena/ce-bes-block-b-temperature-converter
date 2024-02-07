87d058fd713c4053aed83fb0790c5d8d11a3c40c
Jay Millena <jay.millena07@gmail.com>
2024-02-07 15:19:57 +0800

    Initial commit

--- TemperatureConverter.java
+++ TemperatureConverter.java
@@ -6,7 +6,7 @@ public class TemperatureConverter
   {
     double celcius = 0;
     double celcius_fahrenheit = 0;
-    double cercius_kelvin = 0;
+    double celcius_kelvin = 0;
 
     double c2f_con_rate = 33.8; 
     double c2k_con_rate = -272; 
@@ -16,10 +16,10 @@ public class TemperatureConverter
 
     celcius = input.nextDouble();
     celcius_fahrenheit = celcius * c2f_con_rate;
-    cercius_kelvin = celcius * c2k_con_rate;
+    celcius_kelvin = celcius * c2k_con_rate;
 
     System.out.println(celcius +" celcius is "+ celcius_fahrenheit + " fahrenheit");	 
-    System.out.println(celcius +" celcius is "+ cercius_kelvin +" kelvin");
+    System.out.println(celcius +" celcius is "+ celcius_kelvin +" kelvin");
     
     input.close();
   }	
