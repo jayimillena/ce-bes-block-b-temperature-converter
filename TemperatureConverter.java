465ea275d207dd47ab3201052d56a1eb2234af69
Jay Millena <jay.millena07@gmail.com>
2024-02-07 15:14:21 +0800

    git init

---
+++ TemperatureConverter.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class TemperatureConverter
+{
+  public static void main(String[] args)
+  {
+    double celcius = 0;
+    double celcius_fahrenheit = 0;
+    double cercius_kelvin = 0;
+
+    double c2f_con_rate = 33.8; 
+    double c2k_con_rate = -272; 
+
+    Scanner input = new Scanner(System.in);
+    System.out.print("Enter celcius: ");
+
+    celcius = input.nextDouble();
+    celcius_fahrenheit = celcius * c2f_con_rate;
+    cercius_kelvin = celcius * c2k_con_rate;
+
+    System.out.println(celcius +" celcius is "+ celcius_fahrenheit + " fahrenheit");	 
+    System.out.println(celcius +" celcius is "+ cercius_kelvin +" kelvin");
+    
+    input.close();
+  }	
+}
